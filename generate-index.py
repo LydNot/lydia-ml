66ffe06e5c0e09a7a3985dd8ff0790ac2ee41d0c
Lydia Nottingham <65353544+LydNot@users.noreply.github.com>
2025-10-30 01:40:09 -0700

    Auto-deploy: 2025-10-30 01:40:09

--- generate-index.py
+++ generate-index.py
@@ -148,7 +148,7 @@ def generate_index():
 
         <!-- Introduction -->
         <section class="intro">
-            <p>hello! welcome to my site :D i'm a research engineer in san francisco, enrolled in mathphil. i care about good futures and differential technological progress.</p>
+            <p>hello! welcome to my site :D i'm a research engineer in san francisco, enrolled in mathphil. i care about the great present, good futures, and differential technological progress.</p>
         </section>
 
         <!-- Contact -->
